c7abac01d8d16dec9f8b3563cc7abbf9bf821ff5
adham <adhameldaly275@gmail.com>
2026-09-26 12:24:02 +0300

    connect with supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
+import 'package:doctorhunt_app/apps/main/app.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-import 'apps/main/app.dart';
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
-void main() {
-  runApp(const DoctorHuntApp());
+  await Supabase.initialize(
+    url: 'https://xvfbdbptucofajzwccjg.supabase.co',
+    anonKey: 'sb_publishable_hYT78rSC8OuqO6rzoaH8dA_WVoEfYb_',
+  );
+
+  runApp(
+    const DoctorHuntApp(),
+  );
 }
